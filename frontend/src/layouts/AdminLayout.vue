1743dfffc75344b3571432487e220508e3b225fb
fanlingtong <1197228038@qq.com>
2026-09-10 09:57:24 +0800

    修改内容

--- frontend/src/layouts/AdminLayout.vue
+++ frontend/src/layouts/AdminLayout.vue
@@ -28,8 +28,8 @@ const logout = () => {
       <el-menu :default-active="route.path" router :collapse="appStore.sidebarCollapsed">
         <el-menu-item index="/dashboard"><el-icon><Grid /></el-icon><template #title>数据概览</template></el-menu-item>
         <el-menu-item index="/orders"><el-icon><ShoppingCart /></el-icon><template #title>订单管理</template></el-menu-item>
-        <el-menu-item index="/expenses"><el-icon><Wallet /></el-icon><template #title>消费记录</template></el-menu-item>
         <el-menu-item index="/todos"><el-icon><Calendar /></el-icon><template #title>待办事项</template></el-menu-item>
+        <el-menu-item index="/expenses"><el-icon><Wallet /></el-icon><template #title>消费记录</template></el-menu-item>
         <el-menu-item index="/assets"><el-icon><Coin /></el-icon><template #title>资产管理</template></el-menu-item>
         <el-menu-item index="/account-data"><el-icon><DataAnalysis /></el-icon><template #title>账号数据</template></el-menu-item>
         <el-menu-item index="/settings"><el-icon><Setting /></el-icon><template #title>系统设置</template></el-menu-item>
